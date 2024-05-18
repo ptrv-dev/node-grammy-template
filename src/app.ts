eab2f17e1720e331e9227bc813809a2088cc9626
ptrv-dev <petrov99119@gmail.com>
2024-05-18 15:39:31 +0300

    bot error handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { Bot } from 'grammy';
 import { config, logger } from '@/utils';
+import { randomBytes } from 'crypto';
 
 const BOT_TOKEN = config.get('BOT_TOKEN');
 
@@ -9,4 +10,14 @@ bot.start({
   onStart: ({ username }) => {
     logger.success(`Telegram Bot @${username} successfully started.`);
   },
+});
+
+bot.catch(({ ctx, name, message, stack }) => {
+  const code = randomBytes(4).toString('hex');
+  logger.error(
+    `Telegram Bot unhandled error! #${code}\n${name}\n${message}\n${stack}`
+  );
+  ctx.reply(
+    `<b>💢 Непредвиденная ошибка! 💢</b>\n\nВозникла непредвиденная ошибка при работе бота. Пожалуйста попробуйте воспользоваться ботом ещё раз. Если ошибка повторяется обратитесь к тех. поддержке.\n\nКод ошибки: <code>${code}</code>`
+  );
 });
